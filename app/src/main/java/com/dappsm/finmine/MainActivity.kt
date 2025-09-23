0f5243220a794629886c9d4dd830af7a196b00ec
Pedro <114964122+pedrodeleondev@users.noreply.github.com>
2025-09-23 00:34:46 -0600

    Se agregó la tipografía

--- app/src/main/java/com/dappsm/finmine/MainActivity.kt
+++ app/src/main/java/com/dappsm/finmine/MainActivity.kt
@@ -10,8 +10,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.dappsm.finmine.ui.theme.FinmineTheme
+import com.dappsm.finmine.ui.theme.poppinsFontFamily
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +36,9 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        fontFamily = poppinsFontFamily,
+        fontWeight = FontWeight.Bold
     )
 }
 
